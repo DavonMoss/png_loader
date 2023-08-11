a1264c852be97f2595a992ca07378db3a25f8a3a
dmoss <davonlatroymoss@gmail.com>
2023-08-11 16:57:13 -0500

    Added PNG signature verification. Played with bits.

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,69 @@
 #include <iostream>
 #include <fstream>
-#include <sys/stat.h>
+
+/*
+ * VERIFY PNG SIGNATURE
+ *
+ * Checks the first 8 bytes of a file against the expected PNG signature.
+ *
+ * PARAMS:
+ * std::ifstream* file 	- Pointer to Input File Stream object from C++ fstream. Requires file to have already been opened.
+ * 
+ * RETURNS (bool):
+ * TRUE if the signature matches, FALSE if it doesn't.
+ *
+ * */
+bool verify_png_signature(std::ifstream* file) {
+    const unsigned char PNG_SIGNATURE[] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+    int i = 0;
+
+    while(file->good() && i < 8) {
+        if(file->get() != PNG_SIGNATURE[i])
+	    return false;
+
+	i++;
+    }
+    
+    return true;
+}
+
+unsigned long int hex_bytes_to_int(unsigned char* bytes) {
+    // get length of chunk 
+    unsigned long int length = 0;
+
+    for(int i = 0; i < 4; i++) {
+        length = length | bytes[i];	// write the bits of our char onto the bottom 8 bits of the final answer
+	length << 8;			// shift those bits up and make space for the next 8
+    }
+
+    return length;  
+}
 
 int main(int argc, char** argv) {
 
     // take filename as input to CLI
     if (argc != 2) {
         std::cout << "Invalid number of arguments. Give me a filename please!" << std::endl;
+	return -1;
     }
 
     const char * filepath = argv[1];
 
-    // get the filesize in bytes so we know how large to make our buffer for reading the PNG
-    struct stat stat_buffer;
-    stat(filepath, &stat_buffer);
-
-    // creates file stream, opens that file, we specify to read the binary data, then we get every byte one by one
-    // and store it in 'buffer' which has the same size as the file
     std::ifstream file;
     file.open(filepath, std::ios::binary);
-    unsigned char buffer[stat_buffer.st_size];
-    unsigned char c = file.get();
-    buffer[0] = c;
-    int count = 1;
 
-    while(file.good()) {
-	c = file.get();
-	buffer[count];
-	count++;
-    }
+    std::cout << "Signature Validity: " << verify_png_signature(&file) << std::endl;
+    
+    unsigned char char_data[] = {0x00, 0x00, 0x00, 0x0D};
+    unsigned long int hex_data = 0x0000000D;
+    unsigned long int dec_data = 13;
+    
+    std::cout << "Hex == Dec?: " << (hex_data == dec_data) << std::endl;
+    std::cout << "Hex == parse(Char): " << (hex_data == hex_bytes_to_int(char_data)) << std::endl;
+    std::cout << "Hex: " << hex_data << std::endl;
+    std::cout << "hex_bytes_to_int(Char): " << hex_bytes_to_int(char_data) << std::endl;
 
     file.close();
-
-    // Now at this point, we have buffer stored in memory
 
     return 0;
 }
